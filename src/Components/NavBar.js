0b540472c725ccfa517dab90128c84fa57871d22
Ashwini Koli <ashwini.koli1501@gmail.com>
2023-09-30 17:51:31 +0530

    Added some new changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,17 +1,1 @@
-/*import React from "react";*/
 
-/*const Navbar = () => {
-  // Navbar component code
-};
-
-export default Navbar;*/
-const navLinks = document.querySelectorAll("nav a");
-
-navLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = this.getAttribute("href");
-    // Perform navigation functionality here
-    console.log("Navigating to:", href);
-  });
-});
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,18 +1,1 @@
-/*import React from "react";*/
 
-/*const SearchBar = () => {
-  // SearchBar component code
-};
-
-export default SearchBar;*/
-const searchForm = document.querySelector(".search-bar");
-const searchInput = document.querySelector("#textsearch");
-
-searchForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const searchTerm = searchInput.value;
-  // Perform search functionality here
-  console.log("Searching for:", searchTerm);
-  // Clear the search input
-  searchInput.value = "";
-});
